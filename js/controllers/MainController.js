7d34c8c5b0715fe60323114e8fb8bb7810d80ddb
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-01-15 01:00:38 -0500

    added expressions to display info and image on page

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,7 +1,7 @@
 app.controller('MainController', ['$scope', function($scope) {
 	$scope.album = {
 		title: "Smooth Jazz Sounds",
-		cd_img: "images/cd.png",
+		cd_img: "images/cd.jpg",
 		artist: "John Trumpet",
 		release_date: new Date(2015, 05, 08),
 		genre: "Jazz",
